95885d5f45ae22e67fe75bc1ab8d3e646be7bad4
David Stumpe <dstumpe264@gmail.com>
2017-11-11 10:16:22 -0700

    added home method in controller, failed to display index.jsp

--- src/main/java/com/skilldistillery/mvc/controllers/StumpicusController.java
+++ src/main/java/com/skilldistillery/mvc/controllers/StumpicusController.java
@@ -2,6 +2,7 @@ package com.skilldistillery.mvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.skilldistillery.mvc.data.StumpicusDAO;
@@ -11,5 +12,10 @@ import com.skilldistillery.mvc.data.StumpicusDAO;
 public class StumpicusController {
 	@Autowired
 	StumpicusDAO dao;
+	
+	@RequestMapping(path="home.do")
+	public String home() {
+		return "index";
+	}
 
 }
