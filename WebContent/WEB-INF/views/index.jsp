f747c4d56d3df5602c2762f51ba1a17d95b6e196
David Stumpe <dstumpe264@gmail.com>
2017-11-11 11:54:10 -0700

    working with the general

--- WebContent/WEB-INF/views/index.jsp
+++ WebContent/WEB-INF/views/index.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +10,11 @@
 </head>
 <body>
 <h1>Welcome to Lord Stumpicus' Army</h1>
+<h2>Enter your name general</h2>
+<form:form action="general.do" method="post" modelAttribute="general">
+<form:input path="${sessionScope.general}"/><form:errors path="${sessionScope.general}"/>
+<input type="submit" value="Enter"/>
+</form:form>
+
 </body>
 </html>
--- src/main/java/com/skilldistillery/mvc/controllers/StumpicusController.java
+++ src/main/java/com/skilldistillery/mvc/controllers/StumpicusController.java
@@ -2,8 +2,11 @@ package com.skilldistillery.mvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.mvc.data.StumpicusDAO;
 
@@ -13,9 +16,26 @@ public class StumpicusController {
 	@Autowired
 	StumpicusDAO dao;
 	
+	@ModelAttribute("general")
+	public String initSessionObject() {
+		String general = "";
+		return general;
+	}
+	
 	@RequestMapping("home.do")
 	public String home() {
 		return "index";
+	}
+	
+	@RequestMapping("general.do")
+	public ModelAndView getGeneralsName(@RequestParam("name") String userName, 
+								@ModelAttribute("general") String generalsName) {
+		ModelAndView mv = new ModelAndView("intro");
+		generalsName = userName;
+		// Here I want to be able to take the user by his session
+		//and use his name through out the website.
+		//Also i would like to be able to set his army specifically to that user.
+		return mv;
 	}
 
 }
